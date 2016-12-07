7794d5de28b3f3f5981819dbdeb73ca66edfc9fe
hsson <al3xand3r.h@gmail.com>
2016-12-07 19:36:41 +0100

    Makes part 1 cool aswell

--- puzzles/day5/1/main.go
+++ puzzles/day5/1/main.go
@@ -11,15 +11,20 @@ const input = "uqwqemis"
 func main() {
 	count := 0
 	password := ""
+	fmt.Print("DECRYPTING:  ")
 	for len(password) < 8 {
 		data := []byte(input + strconv.Itoa(count))
 		hashed := fmt.Sprintf("%x", md5.Sum(data))
+		char := string(hashed[5])
+		if count%10000 == 0 {
+			// Too slow to print on every try
+			fmt.Printf("\b%v", char)
+		}
 		if hashed[:5] == "00000" {
-			char := string(hashed[5])
 			password += char
-			fmt.Print(char)
+			fmt.Printf("\b%v ", char)
 		}
 		count++
 	}
-	fmt.Println()
+	fmt.Printf("\rDONE:       %v\n", password)
 }
